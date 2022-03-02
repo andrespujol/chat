fafbdd2d75be50f1e8472f858579c3a93b5a6f57
Andrés Pujol <andrespujol@gmail.com>
2022-03-02 17:05:59 -0300

    agregando funcionalidades

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
+const {formatMessage} = require('./utils/utils')
 const PORT = process.env.PORT || 8080;
 const app = express();
 const httServer = http.createServer(app);
 const io = socketIo(httServer);
 
-const messages = [
-    { author: "Juan", text: "¡Hola! ¿Que tal?" },
-    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
-    { author: "Ana", text: "¡Genial!" }
-    ];
-
+const messages = [];
+const users = []
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('./public'))
 
 
+// routes
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+    res.redirect(`./chat?username=${username}`)
+})
+app.get('/chat', (req,res) => {
+    res.sendFile(__dirname + '/public/chat.html')
+})
+
+// listen
 httServer.listen(PORT, ()=> {
     console.log(`Server is up and running on port ${PORT}`)
 })
 
+
+// sockets events
+const botName = 'Chat bot'
 io.on('connection', (socket) => {
     console.log('New client connection');
-    socket.emit('messages', [...messages]);
+    socket.emit('messages', [...messages]); //send messages
+    socket.on('join-chat', ({ username }) => {
+        const newUser = {
+            id:socket.id,
+            username
+        };
+        users.push(newUser)
+
+        // welcome user
+        socket.emit('chat-message', formatMessage(null, botName, 'Welcome'))
+
+        // broadcast user conection
+        socket.broadcast.emit('chat-message', formatMessage(null, botName, `${username} has joined the chat`))
+    })
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,49 @@
 const socket = io();
 
-const messagesContainer = document.getElementById('messagesContainer')
+const messagesContainer = document.getElementById('messagesContainer');
+const usersList = document.getElementById('usersList');
+const chatForm = document.getElementById('chatForm');
+const textInput = document.getElementById('textInput');
+const chatBtn = document.getElementById('chatBtn');
+// const salirBtn = document.getElementById('salirBtn');
 
-socket.on('messages', (data) => {
-    console.log(data)
-    const dataMessage = data.map(message => {
-        let fragment = `<span><b>${message.author}</b>: ${message.text}</span><br>`
-        return fragment
-    }).join(' ')
-    messagesContainer.innerHTML = dataMessage
+const redirectToLogin = () => {
+    window.location = '/';
+};
+
+const renderMessage = (socketId, data) => {
+    const div = document.createElement('div');
+    let className;
+    let html;
+    if (data.id) {
+        if(socketId === data.id) {
+            className = 'myUser-messages';
+            html = `
+            <div id="myUser-message" class="myUser-message">
+            <span><b>Yo:</b> ${data.time}</span>
+            <span>${data.text}</span>
+            </div>`
+        }else {
+            className = 'otherUser-messages';
+            html = `
+            <div id="otherUser-message" class="otherUser-message">
+            <span><b>${data.usename}:</b> ${data.time}</span>
+            <span>${data.text}</span>
+            </div>`
+        }
+    }
+    else {
+        className = 'bot-messages';
+        html = `<span><b>${data.usename} dice:</b> ${data.text}</span>`
+    }
+}
+
+//join chat - con QueryString voy a acceder al usuario.
+const { username } = Qs.parse(window.location.search, {
+    ignoreQueryPrefix: true //para cuando el usuario deja algún espacio en el nombre
+})
+socket.emit('join-chat', { username });
+
+socket.on('chat-message', (data) => {
+
 })
